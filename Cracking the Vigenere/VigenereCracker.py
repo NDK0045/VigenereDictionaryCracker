42b0c89dac97fcb3c79f06b32792a52913a2c829
NDK0045 <bimbimcute05@gmail.com>
2023-11-13 11:32:12 +0700

    Update

---
+++ Cracking the Vigenere/VigenereCracker.py
@@ -0,0 +1,42 @@
+
+
+import detectEnglish, vigenereCipher, pyperclip
+
+def main():
+    ciphertext = """TWT CMGWI ZJTWT AIOEAP XO QT DICJGP MN IWPMR ETCWOCH, SSUHTD, TAETCW, 
+ACS PJFTREW, AVPTRSI JYVEPHZRAQAP WEPGNLEH PYH STXKYRTH, DLAAA YST QT 
+GMOAPEID, PCO RO LPCVACID WHPAW MSHJP, FUI JASN EGZFAQAP GAJHP, 
+WUEEZVTTS MC OPIS SR PUQMRBPEMOC, PYH PPGEMCJALVLN SPWCGXMMNV ISI 
+PAPNI TD QP WEPGNLES, PYH TWT AIRHDYW OG ISMNVH ES BT HPMZTS."""
+    hackedMessage = hackVigenere(ciphertext)
+
+    if hackedMessage != None:
+        print('Copying hacked message to clipboard:')
+        print(hackedMessage)
+        pyperclip.copy(hackedMessage)
+    else:
+        print('Failed to hack encryption.')
+
+
+def hackVigenere(ciphertext):
+    fo = open('dictionary.txt')
+    words = fo.readlines()
+    fo.close()
+
+    for word in words:
+        word = word.strip() # remove the newline at the end
+        decryptedText = vigenereCipher.decryptMessage(word, ciphertext)
+        if detectEnglish.isEnglish(decryptedText, wordPercentage=40):
+            # Check with user to see if the decrypted key has been found.
+            print()
+            print('Possible encryption break:')
+            print('Key ' + str(word) + ': ' + decryptedText[:100])
+            print()
+            print('Enter D for done, or just press Enter to continue breaking:')
+            response = input('> ')
+
+            if response.upper().startswith('D'):
+                return decryptedText
+
+if __name__ == '__main__':
+    main()
